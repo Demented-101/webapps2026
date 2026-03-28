23c85d6f56c6755ff0b8b14bf882b247c89fd6fa
Demented <115271904+Demented-101@users.noreply.github.com>
2026-03-28 11:04:41 +0000

    added home + payapp app

--- webapps2026/urls.py
+++ webapps2026/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from register import views as reg_views
+from payapp import views as payapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", payapp_views.home, name="home"),
+    path("home/", payapp_views.home, name="home"),
 ]
