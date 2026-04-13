f79ea2f0dccf1bee0a6c57958ee41fc8016636b2
Demented <115271904+Demented-101@users.noreply.github.com>
2026-04-13 15:51:08 +0100

    admin registration

--- administation/urls.py
+++ administation/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.admin_homepage, name="adminHomepage"),
     path("ViewUsers", views.view_users, name="Users"),
     path("ViewTransactions", views.view_transactions, name="Transactions"),
     path("ViewTransactionRequests", views.view_transaction_requests, name="TransactionRequests"),
+    path("RegisterAdmin", views.register_new_admin, name="RegisterAdmin"),
 ]
--- administation/views.py
+++ administation/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from administation.models import AdminUser
 from register.models import BalanceUser
 from payapp.models import *
+from django.core.exceptions import ObjectDoesNotExist
 from payapp.views import is_admin
+from django.contrib import messages
+from django.db import transaction, OperationalError
+
+def admin_homepage(request):
+    return render(request, "Administration/home.html", {
+        "registered_by": AdminUser.objects.get(user__username=request.user.username).registered_by,
+        "is_admin": is_admin(request),
+    })
 
 def view_users(request):
     return render(request, "Administration/Users.html", {
         "users": BalanceUser.objects.all(),
+        "admins": AdminUser.objects.all(),
         "is_admin": is_admin(request),
     })
 
@@ -20,3 +31,31 @@ def view_transaction_requests(request):
         "transaction_requests": BalanceTransactionRequest.objects.all(),
         "is_admin": is_admin(request),
     })
+
+def register_new_admin(request):
+    if request.method == "POST":
+        if request.user.is_authenticated and is_admin(request):
+            # get targeted username
+            target_username = request.POST["target_username"]
+            try:
+                target_user = User.objects.get(username=target_username)
+            except ObjectDoesNotExist:
+                messages.error(request, "could not find user with this username")
+                return render(request, "Administration/RegisterAdmin.html", {"is_admin": is_admin(request)})
+
+            ## run transaction atomically
+            try:
+                with transaction.atomic():
+                    AdminUser.objects.create(user=target_user, registered_by=request.user)
+            except OperationalError:
+                messages.error(request, "could not register user as admin.")
+                return render(request, "Administration/RegisterAdmin.html", {"is_admin": is_admin(request)})
+
+            messages.info(request, "user registered as admin successfully")
+            return redirect("/home")
+
+        else: ## not valid user (admin/logged in)
+            messages.error(request, "invalid credentials")
+            return redirect("/home")
+
+    return render(request, "Administration/RegisterAdmin.html", {"is_admin": is_admin(request)})
