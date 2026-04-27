85ddb2669feaa6a1269d11944b88c8ac1cce5322
Demented <115271904+Demented-101@users.noreply.github.com>
2026-04-27 10:10:44 +0100

    allowed hosts change?

--- webapps2026/settings.py
+++ webapps2026/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cmoh2&2=*u5@q*q1^qbdb%txf&t*m&fdw!exti(kc7@*s0km-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
